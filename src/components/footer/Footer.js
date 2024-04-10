158b93224eea62d75da3b933132ff5d25facc47a
Anay Sinhal <91818144+anayy09@users.noreply.github.com>
2024-04-10 16:29:25 +0530

    Updated Footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,16 +1,32 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import "./Footer.scss";
-import {Fade} from "react-reveal";
+import { Fade } from "react-reveal";
 import emoji from "react-easy-emoji";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function Footer() {
-  const {isDark} = useContext(StyleContext);
+  const { isDark } = useContext(StyleContext);
   return (
     <Fade bottom duration={1000} distance="5px">
-      <div className="footer-div">
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Developed with ❤️ by Anay")}
+      <div className="footer-div" style={{ textAlign: "center" }}>
+        <p
+          className={isDark ? "dark-mode footer-text small-text" : "footer-text small-text"}
+        >
+          {emoji("Developed with ❤️ by Anay on ")}
+          <a
+            href="https://react.dev/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React.js
+          </a>
+        </p>
+        <p>
+          "When I'm not coding, you can find me stargazing, plotting my next
+          travel destination, or delving into the fascinating world of
+          geography. I believe in blending the art of programming with the
+          wonders of our universe, making every line of code a journey rather
+          than a task."
         </p>
       </div>
     </Fade>
