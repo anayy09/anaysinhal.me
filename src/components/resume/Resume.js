9464e3265af9a07061df2fa2ff79aec2d005525d
Anay Sinhal <91818144+anayy09@users.noreply.github.com>
2024-04-10 15:57:15 +0530

    Updated Resume for Mobile

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,29 +1,42 @@
 import React, { useEffect } from "react";
 import "./Resume.css";
-// Import the assets directly
-import resumePDF from "./Resume_Anay_Sinhal.pdf";
-import resumePage1 from "./Resume_Anay_Sinhal_01.jpg";
-import resumePage2 from "./Resume_Anay_Sinhal_02.jpg";
 
 const Resume = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
+  // Update the paths to point to the public directory
+  const resumePDF = "Resume_Anay_Sinhal.pdf";
+  const resumePage1 = "Resume_Anay_Sinhal_01.jpg";
+  const resumePage2 = "Resume_Anay_Sinhal_02.jpg";
+
   return (
     <div>
-      <a href={resumePDF} className="download-button" download="Resume_AS.pdf">
+      <a
+        href={process.env.PUBLIC_URL + "/" + resumePDF}
+        className="download-button"
+        download="Resume_AS.pdf"
+      >
         <i className="fa fa-download"></i>Download
       </a>
       <div className="center-frame">
         <iframe
-          src={resumePDF + "#toolbar=0"}
+          src={process.env.PUBLIC_URL + "/" + resumePDF + "#toolbar=0"}
           type="application/pdf"
           title="Anay Sinhal Resume"
           style={{ width: "100%", height: "100vh", border: "none" }}
         ></iframe>
-        <img src={resumePage1} className="mobile-image" alt="Resume Page 1" />
-        <img src={resumePage2} className="mobile-image" alt="Resume Page 2" />
+        <img
+          src={process.env.PUBLIC_URL + "/" + resumePage1}
+          className="mobile-image"
+          alt="Resume Page 1"
+        />
+        <img
+          src={process.env.PUBLIC_URL + "/" + resumePage2}
+          className="mobile-image"
+          alt="Resume Page 2"
+        />
       </div>
     </div>
   );
